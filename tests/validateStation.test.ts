bcfd7a326e90a5a5b22ae8af72c53a1ef548fe63
Matias Nisula <matiasnisula81@gmail.com>
2023-02-01 11:59:42 +0200

    add tests for validating station

--- tests/validateStation.test.ts
+++ tests/validateStation.test.ts
@@ -1,17 +1,31 @@
 import { isValidStation } from "../utils/validateStation";
 
-describe("isValidStation() returns", () => {
+describe("isValidStation(station) returns", () => {
   test("false for station without id", () => {
-    const invalidStation = {
+    const stationWithoutId = {
       name: "Station",
       osoite: "abc A 13",
       kapasiteetti: 10,
       x: "10.2",
       y: "12.3",
     };
-    const result = isValidStation(invalidStation);
+    const result = isValidStation(stationWithoutId);
     expect(result).toBe(false);
   });
+
+  test("false for station with invalid data type id", () => {
+    const stationWithInvalidId = {
+      ID: "abc",
+      name: "Station",
+      osoite: "abc A 13",
+      kapasiteetti: 10,
+      x: "10.2",
+      y: "12.3",
+    };
+    const result = isValidStation(stationWithInvalidId);
+    expect(result).toBe(false);
+  });
+
   test("false for station without name", () => {
     const stationWithoutName = {
       ID: 10,
@@ -23,6 +37,20 @@ describe("isValidStation() returns", () => {
     const result = isValidStation(stationWithoutName);
     expect(result).toBe(false);
   });
+
+  test("false for station with invalid data type name", () => {
+    const stationWithInvalidName = {
+      ID: 10,
+      name: 13,
+      osoite: "abc A 13",
+      kapasiteetti: 10,
+      x: "10.2",
+      y: "12.3",
+    };
+    const result = isValidStation(stationWithInvalidName);
+    expect(result).toBe(false);
+  });
+
   test("false for station without address", () => {
     const stationWithoutAddress = {
       ID: 10,
@@ -34,11 +62,106 @@ describe("isValidStation() returns", () => {
     const result = isValidStation(stationWithoutAddress);
     expect(result).toBe(false);
   });
+
+  test("false for station with invalid data type address", () => {
+    const stationWithInvalidAddress = {
+      ID: 10,
+      name: "abc",
+      osoite: 14,
+      kapasiteetti: 10,
+      x: "10.2",
+      y: "12.3",
+    };
+    const result = isValidStation(stationWithInvalidAddress);
+    expect(result).toBe(false);
+  });
+
+  test("false for station with invalid city data type", () => {
+    const stationWithInvalidCity = {
+      ID: 10,
+      name: "abc",
+      osoite: "abc A",
+      city: 10,
+      kapasiteetti: 10,
+      x: "10.2",
+      y: "12.3",
+    };
+    const result = isValidStation(stationWithInvalidCity);
+    expect(result).toBe(false);
+  });
+  test("false for station with invalid operator data type", () => {
+    const stationWithInvalidOperator = {
+      ID: 10,
+      name: "abc",
+      osoite: "abc A",
+      city: "Helsinki",
+      operaattor: 4,
+      kapasiteetti: 10,
+      x: "10.2",
+      y: "12.3",
+    };
+    const result = isValidStation(stationWithInvalidOperator);
+    expect(result).toBe(false);
+  });
+
+  test("false for station with invalid capacity data type", () => {
+    const stationWithWrongCapacity = {
+      ID: 10,
+      name: "abc",
+      osoite: "abc A",
+      kapasiteetti: "invalid",
+      x: "10.2",
+      y: "12.3",
+    };
+    const result = isValidStation(stationWithWrongCapacity);
+    expect(result).toBe(false);
+  });
+
+  test("false for station with invalid x position data type", () => {
+    const stationWithWrongPositionX = {
+      ID: 10,
+      name: "abc",
+      osoite: "abc A",
+      kapasiteetti: "15",
+      x: "invalid",
+      y: "10",
+    };
+    const result = isValidStation(stationWithWrongPositionX);
+    expect(result).toBe(false);
+  });
+
+  test("false for station with invalid y position data type", () => {
+    const stationWithWrongPositionY = {
+      ID: 10,
+      name: "abc",
+      osoite: "abc A",
+      kapasiteetti: "15",
+      x: "10",
+      y: "invalid",
+    };
+    const result = isValidStation(stationWithWrongPositionY);
+    expect(result).toBe(false);
+  });
+
+  test("true for station with missing capacity, city and operator", () => {
+    const stationWithoutCapacity = {
+      ID: 10,
+      name: "abc",
+      osoite: "abc A",
+      x: "10",
+      y: "11",
+    };
+    const result = isValidStation(stationWithoutCapacity);
+    expect(result).toBe(true);
+  });
+
   test("true for valid station", () => {
     const validStation = {
       ID: 10,
       name: "abc",
       osoite: "abc A",
+      city: "helsinki",
+      operaattor: "aaaa",
       kapasiteetti: 10,
       x: "10.2",
       y: "12.3",
