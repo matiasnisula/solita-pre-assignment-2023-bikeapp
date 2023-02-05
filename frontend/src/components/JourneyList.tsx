5e5c11a536c1fab84f739c3722c555ee1d65ea73
Matias Nisula <matiasnisula81@gmail.com>
2023-02-05 17:27:59 +0200

    add header to JourneyList component

--- frontend/src/components/JourneyList.tsx
+++ frontend/src/components/JourneyList.tsx
@@ -1,35 +1,42 @@
 import { JourneyEntry } from "../types";
 import Journey from "./Journey";
 
-const JourneyList = ({ journeyList }: { journeyList: Array<JourneyEntry> }) => {
+interface JourneyListProps {
+  journeyList: Array<JourneyEntry>;
+}
+
+const JourneyList = ({ journeyList }: JourneyListProps) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>Departure station</th>
-          <th>Return station</th>
-          <th>Covered distance</th>
-          <th>Duration</th>
-        </tr>
-      </thead>
-      <tbody>
-        {journeyList.map((journey) => {
-          return (
-            <Journey
-              key={journey.id}
-              id={journey.id}
-              departureDate={journey.departureDate}
-              returnDate={journey.returnDate}
-              departureStation={journey.departureStation}
-              returnStation={journey.returnStation}
-              coveredDistance={journey.coveredDistance}
-              duration={journey.duration}
-            />
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <h1>Journeys</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Departure station</th>
+            <th>Return station</th>
+            <th>Covered distance</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          {journeyList.map((journey) => {
+            return (
+              <Journey
+                key={journey.id}
+                id={journey.id}
+                departureDate={journey.departureDate}
+                returnDate={journey.returnDate}
+                departureStation={journey.departureStation}
+                returnStation={journey.returnStation}
+                coveredDistance={journey.coveredDistance}
+                duration={journey.duration}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
