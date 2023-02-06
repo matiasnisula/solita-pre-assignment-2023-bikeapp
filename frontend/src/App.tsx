deb4b6dc0361e2ac343a69e0330c2fe2aebd93bb
Matias Nisula <matiasnisula81@gmail.com>
2023-02-06 12:33:31 +0200

    add basic routing for journeys and stations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import JourneyList from "./components/JourneyList";
 import StationList from "./components/StationList";
 
 const App = () => {
+  const padding = {
+    padding: 5,
+  };
   return (
-    <div>
-      <JourneyList />
-      <StationList />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Link style={padding} to="/journeys">
+          journeys
+        </Link>
+        <Link style={padding} to="/stations">
+          stations
+        </Link>
+      </div>
+      <Routes>
+        <Route path="/journeys" element={<JourneyList />} />
+        <Route path="/stations" element={<StationList />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
